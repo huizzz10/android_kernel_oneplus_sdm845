MotorolaMobilityLLC/kernel-msm

msm: vidc: Remove devfreq module from video
Bus bandwidth voting can be achieved by directly call
the bus apis. DevFreq functionality is not utilized nor
necessary. Also, video bus bandwidth votes are dynamic
and cannot conform to the OPP table entries.

CRs-Fixed: 2384822
Change-Id: Ib83f51d7d07efe24a256e0287433e7a28e2ae057
Signed-off-by: Chinmay Sawarkar <chinmays@codeaurora.org>
Signed-off-by: Vikash Garodia <vgarodia@codeaurora.org>

drivers/media/platform/msm/vidc/venus_hfi.c
@@ -8,7 +8,6 @@
 #include <linux/clk/qcom.h>
 #include <linux/coresight-stm.h>
 #include <linux/delay.h>
-#include <linux/devfreq.h>
 #include <linux/hash.h>
 #include <linux/interrupt.h>
 #include <linux/io.h>
@@ -984,84 +983,24 @@ static void __set_threshold_registers(struct venus_hfi_device *device)
 		dprintk(VIDC_ERR, "Failed to restore threshold values\n");
 }
 
-static int __devfreq_target(struct device *devfreq_dev,
-		unsigned long *freq, u32 flags)
+static int __vote_bandwidth(struct bus_info *bus,
+		unsigned long *freq)
 {
 	int rc = 0;
 	uint64_t ab = 0;
-	struct bus_info *bus = NULL, *temp = NULL;
-	struct venus_hfi_device *device = dev_get_drvdata(devfreq_dev);
 
-	venus_hfi_for_each_bus(device, temp) {
-		if (temp->dev == devfreq_dev) {
-			bus = temp;
-			break;
-		}
-	}
-
-	if (!bus) {
-		rc = -EBADHANDLE;
-		goto err_unknown_device;
-	}
-
-	/*
-	 * Clamp for all non zero frequencies. This clamp is necessary to stop
-	 * devfreq driver from spamming - Couldn't update frequency - logs, if
-	 * the scaled ab value is not part of the frequency table.
-	 */
 	if (*freq)
 		*freq = clamp_t(typeof(*freq), *freq, bus->range[0],
 				bus->range[1]);
 
-	/* we expect governors to provide values in kBps form, convert to Bps */
+	/* Bus Driver expects values in Bps */
 	ab = *freq * 1000;
-	rc = msm_bus_scale_update_bw(bus->client, ab, 0);
-	if (rc) {
-		dprintk(VIDC_ERR, "Failed voting bus %s to ab %llu\n: %d",
-				bus->name, ab, rc);
-		goto err_unknown_device;
-	}
-
 	dprintk(VIDC_PROF, "Voting bus %s to ab %llu\n", bus->name, ab);
+	rc = msm_bus_scale_update_bw(bus->client, ab, 0);
+	if (rc)
+		dprintk(VIDC_ERR, "Failed voting bus %s to ab %llu, rc=%d\n",
+				bus->name, ab, rc);
 
-	return 0;
-err_unknown_device:
-	return rc;
-}
-
-static int __devfreq_get_status(struct device *devfreq_dev,
-		struct devfreq_dev_status *stat)
-{
-	int rc = 0;
-	struct bus_info *bus = NULL, *temp = NULL;
-	struct venus_hfi_device *device = dev_get_drvdata(devfreq_dev);
-
-	venus_hfi_for_each_bus(device, temp) {
-		if (temp->dev == devfreq_dev) {
-			bus = temp;
-			break;
-		}
-	}
-
-	if (!bus) {
-		rc = -EBADHANDLE;
-		goto err_unknown_device;
-	}
-
-	*stat = (struct devfreq_dev_status) {
-		.private_data = &device->bus_vote,
-		/*
-		 * Put in dummy place holder values for upstream govs, our
-		 * custom gov only needs .private_data.  We should fill this in
-		 * properly if we can actually measure busy_time accurately
-		 * (which we can't at the moment)
-		 */
-		.total_time = 1,
-		.busy_time = 1,
-		.current_frequency = 0,
-	};
-
-err_unknown_device:
 	return rc;
 }
 
@@ -1069,18 +1008,19 @@ static int __unvote_buses(struct venus_hfi_device *device)
 {
 	int rc = 0;
 	struct bus_info *bus = NULL;
+	unsigned long freq = 0, zero = 0;
 
 	kfree(device->bus_vote.data);
 	device->bus_vote.data = NULL;
 	device->bus_vote.data_count = 0;
 
 	venus_hfi_for_each_bus(device, bus) {
-		unsigned long zero = 0;
-
-		if (!bus->is_prfm_gov_used)
-			rc = devfreq_suspend_device(bus->devfreq);
+		if (!bus->is_prfm_gov_used) {
+			freq = __calc_bw(bus, &device->bus_vote);
+			rc = __vote_bandwidth(bus, &freq);
+		}
 		else
-			rc = __devfreq_target(bus->dev, &zero, 0);
+			rc = __vote_bandwidth(bus, &zero);
 
 		if (rc)
 			goto err_unknown_device;
@@ -1096,6 +1036,7 @@ static int __vote_buses(struct venus_hfi_device *device,
 	int rc = 0;
 	struct bus_info *bus = NULL;
 	struct vidc_bus_vote_data *new_data = NULL;
+	unsigned long freq = 0;
 
 	if (!num_data) {
 		dprintk(VIDC_DBG, "No vote data available\n");
@@ -1118,15 +1059,18 @@ no_data_count:
 	device->bus_vote.data_count = num_data;
 
 	venus_hfi_for_each_bus(device, bus) {
-		if (bus && bus->devfreq) {
+		if (bus) {
 			if (!bus->is_prfm_gov_used) {
-				rc = devfreq_resume_device(bus->devfreq);
-				if (rc)
-					goto err_no_mem;
+				freq = __calc_bw(bus, &device->bus_vote);
 			} else {
-				bus->devfreq->nb.notifier_call(
-					&bus->devfreq->nb, 0, NULL);
+				freq = bus->range[1];
+				dprintk(VIDC_ERR, "%s %s perf Vote %u\n",
+						__func__, bus->name,
+						bus->range[1]);
 			}
+			rc = __vote_bandwidth(bus, &freq);
+		} else {
+			dprintk(VIDC_ERR, "No BUS to Vote\n");
 		}
 	}
 
@@ -3891,10 +3835,6 @@ static void __deinit_bus(struct venus_hfi_device *device)
 	device->bus_vote = DEFAULT_BUS_VOTE;
 
 	venus_hfi_for_each_bus_reverse(device, bus) {
-		devfreq_remove_device(bus->devfreq);
-		bus->devfreq = NULL;
-		dev_set_drvdata(bus->dev, NULL);
-
 		msm_bus_scale_unregister(bus->client);
 		bus->client = NULL;
 	}
@@ -3909,41 +3849,14 @@ static int __init_bus(struct venus_hfi_device *device)
 		return -EINVAL;
 
 	venus_hfi_for_each_bus(device, bus) {
-		struct devfreq_dev_profile profile = {
-			.initial_freq = 0,
-			.polling_ms = INT_MAX,
-			.freq_table = NULL,
-			.max_state = 0,
-			.target = __devfreq_target,
-			.get_dev_status = __devfreq_get_status,
-			.exit = NULL,
-			/*.get_cur_greq = NULL,*/
-		};
-
-		if (!strcmp(bus->governor, "msm-vidc-llcc")) {
+		if (!strcmp(bus->mode, "msm-vidc-llcc")) {
 			if (msm_vidc_syscache_disable) {
 				dprintk(VIDC_DBG,
 					 "Skipping LLC bus init %s: %s\n",
-				bus->name, bus->governor);
+				bus->name, bus->mode);
 				continue;
 			}
 		}
-
-		/*
-		 * This is stupid, but there's no other easy way to get a hold
-		 * of struct bus_info in venus_hfi_devfreq_*()
-		 */
-		WARN(dev_get_drvdata(bus->dev), "%s's drvdata already set\n",
-				dev_name(bus->dev));
-		dev_set_drvdata(bus->dev, device);
-
-		if (bus->has_freq_table) {
-			rc = dev_pm_opp_of_add_table(bus->dev);
-			if (rc)
-				dprintk(VIDC_ERR, "Failed to add %s OPP table",
-						bus->name);
-		}
-
 		bus->client = msm_bus_scale_register(bus->master, bus->slave,
 				bus->name, false);
 		if (IS_ERR_OR_NULL(bus->client)) {
@@ -3954,25 +3867,6 @@ static int __init_bus(struct venus_hfi_device *device)
 			bus->client = NULL;
 			goto err_add_dev;
 		}
-
-		bus->devfreq_prof = profile;
-		bus->devfreq = devfreq_add_device(bus->dev,
-				&bus->devfreq_prof, bus->governor, NULL);
-		if (IS_ERR_OR_NULL(bus->devfreq)) {
-			rc = PTR_ERR(bus->devfreq) ?
-				PTR_ERR(bus->devfreq) : -EBADHANDLE;
-			dprintk(VIDC_ERR,
-					"Failed to add devfreq device for bus %s and governor %s: %d\n",
-					bus->name, bus->governor, rc);
-			bus->devfreq = NULL;
-			goto err_add_dev;
-		}
-
-		/*
-		 * Devfreq starts monitoring immediately, since we are just
-		 * initializing stuff at this point, force it to suspend
-		 */
-		devfreq_suspend_device(bus->devfreq);
 	}
 
 	return 0;

huizzz10/android_kernel_oneplus_sdm845

drivers/media/platform/msm/vidc/venus_hfi.c
@@ -16,7 +16,6 @@
 #include <linux/clk/qcom.h>
 #include <linux/coresight-stm.h>
 #include <linux/delay.h>
-#include <linux/devfreq.h>
 #include <linux/hash.h>
 #include <linux/interrupt.h>
 #include <linux/io.h>
@@ -985,84 +984,24 @@ static void __set_threshold_registers(struct venus_hfi_device *device)
 		dprintk(VIDC_ERR, "Failed to restore threshold values\n");
 }
 
-static int __devfreq_target(struct device *devfreq_dev,
-		unsigned long *freq, u32 flags)
+static int __vote_bandwidth(struct bus_info *bus,
+		unsigned long *freq)
 {
 	int rc = 0;
 	uint64_t ab = 0;
-	struct bus_info *bus = NULL, *temp = NULL;
-	struct venus_hfi_device *device = dev_get_drvdata(devfreq_dev);
 
-	venus_hfi_for_each_bus(device, temp) {
-		if (temp->dev == devfreq_dev) {
-			bus = temp;
-			break;
-		}
-	}
-
-	if (!bus) {
-		rc = -EBADHANDLE;
-		goto err_unknown_device;
-	}
-
-	/*
-	 * Clamp for all non zero frequencies. This clamp is necessary to stop
-	 * devfreq driver from spamming - Couldn't update frequency - logs, if
-	 * the scaled ab value is not part of the frequency table.
-	 */
 	if (*freq)
 		*freq = clamp_t(typeof(*freq), *freq, bus->range[0],
 				bus->range[1]);
 
-	/* we expect governors to provide values in kBps form, convert to Bps */
+	/* Bus Driver expects values in Bps */
 	ab = *freq * 1000;
-	rc = msm_bus_scale_update_bw(bus->client, ab, 0);
-	if (rc) {
-		dprintk(VIDC_ERR, "Failed voting bus %s to ab %llu\n: %d",
-				bus->name, ab, rc);
-		goto err_unknown_device;
-	}
-
 	dprintk(VIDC_PROF, "Voting bus %s to ab %llu\n", bus->name, ab);
+	rc = msm_bus_scale_update_bw(bus->client, ab, 0);
+	if (rc)
+		dprintk(VIDC_ERR, "Failed voting bus %s to ab %llu, rc=%d\n",
+				bus->name, ab, rc);
 
-	return 0;
-err_unknown_device:
-	return rc;
-}
-
-static int __devfreq_get_status(struct device *devfreq_dev,
-		struct devfreq_dev_status *stat)
-{
-	int rc = 0;
-	struct bus_info *bus = NULL, *temp = NULL;
-	struct venus_hfi_device *device = dev_get_drvdata(devfreq_dev);
-
-	venus_hfi_for_each_bus(device, temp) {
-		if (temp->dev == devfreq_dev) {
-			bus = temp;
-			break;
-		}
-	}
-
-	if (!bus) {
-		rc = -EBADHANDLE;
-		goto err_unknown_device;
-	}
-
-	*stat = (struct devfreq_dev_status) {
-		.private_data = &device->bus_vote,
-		/*
-		 * Put in dummy place holder values for upstream govs, our
-		 * custom gov only needs .private_data.  We should fill this in
-		 * properly if we can actually measure busy_time accurately
-		 * (which we can't at the moment)
-		 */
-		.total_time = 1,
-		.busy_time = 1,
-		.current_frequency = 0,
-	};
-
-err_unknown_device:
 	return rc;
 }
 
@@ -1070,21 +1009,19 @@ static int __unvote_buses(struct venus_hfi_device *device)
 {
 	int rc = 0;
 	struct bus_info *bus = NULL;
+	unsigned long freq = 0, zero = 0;
 
 	kfree(device->bus_vote.data);
 	device->bus_vote.data = NULL;
 	device->bus_vote.data_count = 0;
 
 	venus_hfi_for_each_bus(device, bus) {
-		unsigned long zero = 0;
-
 		if (!bus->is_prfm_gov_used) {
-			mutex_lock(&bus->devfreq->lock);
-			rc = update_devfreq(bus->devfreq);
-			mutex_unlock(&bus->devfreq->lock);
+			freq = __calc_bw(bus, &device->bus_vote);
+			rc = __vote_bandwidth(bus, &freq);
 		}
 		else
-			rc = __devfreq_target(bus->dev, &zero, 0);
+			rc = __vote_bandwidth(bus, &zero);
 
 		if (rc)
 			goto err_unknown_device;
@@ -1100,6 +1037,7 @@ static int __vote_buses(struct venus_hfi_device *device,
 	int rc = 0;
 	struct bus_info *bus = NULL;
 	struct vidc_bus_vote_data *new_data = NULL;
+	unsigned long freq = 0;
 
 	if (!num_data) {
 		dprintk(VIDC_DBG, "No vote data available\n");
@@ -1122,17 +1060,18 @@ no_data_count:
 	device->bus_vote.data_count = num_data;
 
 	venus_hfi_for_each_bus(device, bus) {
-		if (bus && bus->devfreq) {
+		if (bus) {
 			if (!bus->is_prfm_gov_used) {
-				mutex_lock(&bus->devfreq->lock);
-				rc = update_devfreq(bus->devfreq);
-				mutex_unlock(&bus->devfreq->lock);
-				if (rc)
-					goto err_no_mem;
+				freq = __calc_bw(bus, &device->bus_vote);
 			} else {
-				bus->devfreq->nb.notifier_call(
-					&bus->devfreq->nb, 0, NULL);
+				freq = bus->range[1];
+				dprintk(VIDC_DBG, "%s %s perf Vote %u\n",
+						__func__, bus->name,
+						bus->range[1]);
 			}
+			rc = __vote_bandwidth(bus, &freq);
+		} else {
+			dprintk(VIDC_ERR, "No BUS to Vote\n");
 		}
 	}
 
@@ -4071,10 +4010,6 @@ static void __deinit_bus(struct venus_hfi_device *device)
 	device->bus_vote = DEFAULT_BUS_VOTE;
 
 	venus_hfi_for_each_bus_reverse(device, bus) {
-		devfreq_remove_device(bus->devfreq);
-		bus->devfreq = NULL;
-		dev_set_drvdata(bus->dev, NULL);
-
 		msm_bus_scale_unregister(bus->client);
 		bus->client = NULL;
 	}
@@ -4089,33 +4024,14 @@ static int __init_bus(struct venus_hfi_device *device)
 		return -EINVAL;
 
 	venus_hfi_for_each_bus(device, bus) {
-		struct devfreq_dev_profile profile = {
-			.initial_freq = 0,
-			.polling_ms = INT_MAX,
-			.freq_table = NULL,
-			.max_state = 0,
-			.target = __devfreq_target,
-			.get_dev_status = __devfreq_get_status,
-			.exit = NULL,
-		};
-
-		if (!strcmp(bus->governor, "msm-vidc-llcc")) {
+		if (!strcmp(bus->mode, "msm-vidc-llcc")) {
 			if (msm_vidc_syscache_disable) {
 				dprintk(VIDC_DBG,
 					 "Skipping LLC bus init %s: %s\n",
-				bus->name, bus->governor);
+				bus->name, bus->mode);
 				continue;
 			}
 		}
-
-		/*
-		 * This is stupid, but there's no other easy way to ahold
-		 * of struct bus_info in venus_hfi_devfreq_*()
-		 */
-		WARN(dev_get_drvdata(bus->dev), "%s's drvdata already set\n",
-				dev_name(bus->dev));
-		dev_set_drvdata(bus->dev, device);
-
 		bus->client = msm_bus_scale_register(bus->master, bus->slave,
 				bus->name, false);
 		if (IS_ERR_OR_NULL(bus->client)) {
@@ -4125,24 +4041,6 @@ static int __init_bus(struct venus_hfi_device *device)
 			bus->client = NULL;
 			goto err_add_dev;
 		}
-
-		bus->devfreq_prof = profile;
-		bus->devfreq = devfreq_add_device(bus->dev,
-				&bus->devfreq_prof, bus->governor, NULL);
-		if (IS_ERR_OR_NULL(bus->devfreq)) {
-			rc = PTR_ERR(bus->devfreq) ?: -EBADHANDLE;
-			dprintk(VIDC_ERR,
-					"Failed to add devfreq device for bus %s and governor %s: %d\n",
-					bus->name, bus->governor, rc);
-			bus->devfreq = NULL;
-			goto err_add_dev;
-		}
-
-		/*
-		 * Devfreq starts monitoring immediately, since we are just
-		 * initializing stuff at this point, force it to suspend
-		 */
-		devfreq_suspend_device(bus->devfreq);
 	}
 
 	return 0;